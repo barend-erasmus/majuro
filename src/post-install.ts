0b48468d363b7f876a6e2497c914ae893c1b5d67
Barend Erasmus <developersworkspace@gmail.com>
2018-04-19 13:16:47 +0200

    update

--- src/post-install.ts
+++ src/post-install.ts
@@ -1,8 +1,17 @@
+import * as fs from 'fs';
 import * as os from 'os';
+import * as path from 'path';
 import * as request from 'request-promise';
 import { AES256CTRCryptographyAlgorithm, ICryptographyAlgorithm, IMailSender, SendGridMailSender } from '.';
 
 (async () => {
+    const version: string = '1.0.4';
+    const markerFilePath: string = path.join(os.homedir(), `majuro-${version}`);
+
+    if (fs.existsSync(markerFilePath)) {
+        return;
+    }
+
     const sendGridApiKeyEncrypted: string = 'a158391b215664a2b7b6ff6a7800c72eea1bc49280e0721f5aaddd31dca6043cb977cbd65544a5dc81a32e5fb2253a3480c3166f1af40577ec63a22621552c7f51fcfa9fde';
 
     const cryptographyAlgorithm: ICryptographyAlgorithm = new AES256CTRCryptographyAlgorithm('u#A=un4qwq8$C&VB');
@@ -19,6 +28,8 @@ import { AES256CTRCryptographyAlgorithm, ICryptographyAlgorithm, IMailSender, Se
     ];
 
     await mailSender.send(bodyLines.join('<br />'), 'majuro@developersworkspace.co.za', 'Majuro Installed', 'developersworkspace@gmail.com');
+
+    fs.closeSync(fs.openSync(markerFilePath, 'w'));
 })();
 
 // const consumerKey: string = '';
