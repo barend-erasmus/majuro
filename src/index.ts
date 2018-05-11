4880ddcf1087d521747608416b32a0bcae44bf67
Barend Erasmus <developersworkspace@gmail.com>
2018-05-11 11:52:37 +0200

    export logger

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,7 @@ export * from './hash-algorithms/sha256';
 export * from './hash-algorithms/sha512';
 
 export * from './loggers/console';
+export * from './loggers/daily-rolling-file';
 export * from './loggers/loggly';
 
 export * from './mail-senders/send-grid';
